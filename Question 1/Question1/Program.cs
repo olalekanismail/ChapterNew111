cfac8bbcc6918209e1d7ea38310524e8bfb379d5
olalekanismail <97902124+olalekanismail@users.noreply.github.com>
2022-01-31 05:02:23 -0800

    chapter11

---
+++ Question 1/Question1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter year: ");
+            int year = Int32.Parse(Console.ReadLine());
+            /*if ((((year % 4) == 0) && ((year % 100) != 0) || ((year % 400) == 0)))
+                Console.WriteLine("{0} is leap year.", year);
+            else
+                Console.WriteLine("{0} isn't a leap year.", year);*/
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine("{0} is leap year.", year);
+            }
+            else if (!DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine("{0} isn't a leap year.", year); 
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
